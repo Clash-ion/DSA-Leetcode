53cc382d6a60f3fb039fad2967542c455ade77f7
Mukul Kumar <mukul0000kumar@gmail.com>
2022-03-14 10:45:36 +0530

    Add floor.java

---
+++ normal practice/binary search questions/floor.java
@@ -0,0 +1,24 @@
+public class floor {
+
+    static int binarySearch(int[] arr, int x){
+        int start = 0, end = arr.length -1,mid;
+        while (start <= end) {
+            mid = start + (end - start) / 2;
+            if (arr[mid] == x){
+                return mid;
+            }
+            if (arr[mid] < x){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        return end;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {0,1,2,4,5,6,7,8,9};
+        System.out.println(binarySearch(arr, 3));
+    }
+}
