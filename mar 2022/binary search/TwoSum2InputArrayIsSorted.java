b81e76a66e5f5a30df416512c24095aa32d238e7
Mukul Kumar <mukul0000kumar@gmail.com>
2022-03-16 10:14:22 +0530

    Add TwoSum2InputArrayIsSorted.java

---
+++ mar 2022/binary search/TwoSum2InputArrayIsSorted.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class TwoSum2InputArrayIsSorted {
+    static int[] twoSum(int[] numbers, int target) {
+        int start = 0, end = numbers.length - 1, sum = 0;
+        while(start < end){
+            sum = numbers[start] + numbers[end];
+            if (sum == target){
+                return new int[] {start + 1, end + 1};
+            }
+            else if(sum > target){
+                end--;
+            }
+            else{
+                start++;
+            }
+        }
+        return new int[] {-1,-1};
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2,4,11,15};
+        System.out.println(Arrays.toString(twoSum(arr, 13)));
+    }
+
+}
